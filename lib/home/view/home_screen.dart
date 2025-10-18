674db105b704df693078d075897526836d1ffbd2
Ebrahim Hossain <ebrahimhossain626@gmail.com>
2025-10-18 21:09:52 +0600

    updated

--- lib/home/view/home_screen.dart
+++ lib/home/view/home_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context,ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text(
+            'Collections',
+          style: TextStyle(fontWeight: FontWeight.w700),
+        )),
+      ),
+      body: ,
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:state_notifier_provider/home/view/home_screen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
